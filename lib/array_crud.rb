5ee28ac09362e01c23926c977e8c921549ed287f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-04 14:37:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,33 +8,31 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    arr=['aa','bb','cc','dd']
-    arr << 'ee'
+
+    arr << element
 end
 
 def add_element_to_start_of_array(array, element)
-  arr=['aa','bb','cc','dd']
-  arr.unshift('zz')
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-      arr=['aa','bb','cc','dd']
+
       arr.pop()
 end
 
 def remove_element_from_start_of_array(array)
-      arr=['aa','bb','cc','dd']
       arr.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-     
+     array[insex_nuber]
 end
 
 def retrieve_first_element_from_array(array)
-
+   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+      array[-1]
 end
