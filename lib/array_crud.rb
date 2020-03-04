b14c4613c4b42a62cef365fd4b94991421958745
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-04 14:21:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,4 @@
+
 def create_an_empty_array
   arr=[]
 end
@@ -12,19 +13,22 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-
+  arr=['aa','bb','cc','dd']
+  arr.unshift('zz')
 end
 
 def remove_element_from_end_of_array(array)
-
+      arr=['aa','bb','cc','dd']
+      arr.pop()    
 end
 
 def remove_element_from_start_of_array(array)
-
+      arr=['aa','bb','cc','dd']
+      arr.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-
+     
 end
 
 def retrieve_first_element_from_array(array)
